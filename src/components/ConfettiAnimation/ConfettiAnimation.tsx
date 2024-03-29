db4f6223dfb9c92f00215a32a0f29b62eb7ca84b
kadirosmanust <kadirosmanust98@gmail.com>
2024-03-29 18:40:46 +0300

    feat: add confetti animation

---
+++ src/components/ConfettiAnimation/ConfettiAnimation.tsx
@@ -0,0 +1,42 @@
+import Lottie from 'react-lottie';
+import confettiAnimationData from '@/assets/lottieFiles/confettiAnimation.json';
+import styles from './ConfettiAnimation.module.scss';
+import { useEffect, useState } from 'react';
+
+type ConfettiAnimationProps = {
+  isStopped: boolean;
+};
+
+const ConfettiAnimation = ({
+  isStopped,
+}: ConfettiAnimationProps): JSX.Element => {
+  const [isDone, setIsDone] = useState(true);
+
+  useEffect(() => {
+    if (!isStopped) {
+      setTimeout(() => {
+        setIsDone(true);
+      }, 5000);
+    }
+
+    if (isStopped) {
+      setIsDone(false);
+    }
+  }, [isStopped]);
+
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: confettiAnimationData,
+    rendererSettings: {
+      preserveAspectRatio: 'xMidYMid slice',
+    },
+  };
+  return (
+    <div className={styles.container}>
+      <Lottie options={defaultOptions} isStopped={isStopped || isDone} />
+    </div>
+  );
+};
+
+export default ConfettiAnimation;
