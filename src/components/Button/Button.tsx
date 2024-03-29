ab94951ccb52276f64c78eb2cfd1ef8378aa59f6
kadirosmanust <kadirosmanust98@gmail.com>
2024-03-29 18:38:53 +0300

    feat: add button

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import styles from './Button.module.scss';
+
+interface ButtonProps extends React.HTMLAttributes<HTMLButtonElement> {
+  children?: React.ReactNode;
+  htmlType?: 'button' | 'submit' | 'reset';
+}
+
+const Button = ({ children, ...props }: ButtonProps): JSX.Element => {
+  return (
+    <button {...props} className={styles.button}>
+      {children}
+    </button>
+  );
+};
+
+export default Button;
