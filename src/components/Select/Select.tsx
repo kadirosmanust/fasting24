473c71118fa83b6200e941938522806e2d195cf3
kadirosmanust <kadirosmanust98@gmail.com>
2024-03-29 18:39:19 +0300

    feat: add time select

---
+++ src/components/Select/Select.tsx
@@ -0,0 +1,110 @@
+/* eslint-disable no-unused-vars */
+import { useEffect, useRef, useState } from 'react';
+import styles from './Select.module.scss';
+import { hours, minutes } from '@/constants/times';
+
+type SelectProps = {
+  onChange: (value: { hour: string | null; minute: string | null }) => void;
+  value: { hour: string | null; minute: string | null };
+  disabled?: boolean;
+};
+
+const Select = ({ value, onChange, disabled }: SelectProps): JSX.Element => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [selectedHour, setSelectedHour] = useState<string | null>(value.hour);
+  const [selectedMinute, setSelectedMinute] = useState<string | null>(
+    value.minute,
+  );
+
+  useEffect(() => {
+    setSelectedHour(value.hour);
+    setSelectedMinute(value.minute);
+  }, [value]);
+
+  const selectRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (
+        selectRef.current &&
+        !selectRef.current.contains(event.target as Node)
+      ) {
+        setIsOpen(false);
+      }
+    };
+
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, []);
+
+  const handleWithHour = (hour: string) => {
+    setSelectedHour(hour);
+    onChange({
+      hour,
+      minute: selectedMinute,
+    });
+  };
+
+  const handleWithMinute = (minute: string) => {
+    setSelectedMinute(minute);
+    onChange({
+      hour: selectedHour,
+      minute: minute,
+    });
+  };
+
+  return (
+    <div
+      style={{
+        pointerEvents: disabled ? 'none' : 'auto',
+        cursor: disabled ? 'not-allowed' : 'pointer',
+      }}
+      className={styles.customSelect}
+      ref={selectRef}
+    >
+      <div
+        className={`${styles.selectedOption} ${isOpen ? styles.open : ''}`}
+        onClick={() => setIsOpen(!isOpen)}
+      >
+        <span>
+          {selectedHour} : {selectedMinute}
+        </span>
+      </div>
+
+      {isOpen && (
+        <div className={styles.optionsContainer}>
+          <div className={styles.optionsItemList}>
+            {hours
+              .map(hour => ({ value: hour, label: `${hour}` }))
+              .map((option, idx) => (
+                <div
+                  key={idx}
+                  className={styles.option}
+                  onClick={() => handleWithHour(option.value)}
+                >
+                  {option.label}
+                </div>
+              ))}
+          </div>
+          <div className={styles.optionsItemList}>
+            {minutes
+              .map(hour => ({ value: hour, label: `${hour}` }))
+              .map((option, idx) => (
+                <div
+                  key={idx}
+                  className={styles.option}
+                  onClick={() => handleWithMinute(option.value)}
+                >
+                  {option.label}
+                </div>
+              ))}
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Select;
