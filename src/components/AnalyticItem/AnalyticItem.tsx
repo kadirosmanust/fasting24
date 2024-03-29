f09cdb857ce63e94940754ec75ed0850d509d9ce
kadirosmanust <kadirosmanust98@gmail.com>
2024-03-29 18:41:40 +0300

    feat: analytic

---
+++ src/components/AnalyticItem/AnalyticItem.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import styles from './AnalyticItem.module.scss';
+
+type AnalyticItemProps = {
+  icon?: React.ReactNode;
+  title: string;
+  description: string;
+};
+
+const AnalyticItem = ({
+  icon,
+  title,
+  description,
+}: AnalyticItemProps): JSX.Element => {
+  return (
+    <div className={styles.analyticItem}>
+      {icon && <span>{icon}</span>}
+      <div>
+        <h3>{title ?? ''}</h3>
+        <p>{description ?? ''}</p>
+      </div>
+    </div>
+  );
+};
+
+export default AnalyticItem;
